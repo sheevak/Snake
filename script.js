058f30bca2319867a0d2ef19c1882a8bf4c0dbfa
Sheeva Kavousian <sheevakavous@gmail.com>
2021-01-07 15:07:05 +0000

    create a canvas element as the game area

---
+++ script.js
@@ -0,0 +1,6 @@
+let gameArea = document.createElement("canvas");
+gameArea.width = 500;
+gameArea.height = 500;
+gameArea.style.background = "black";
+let context = gameArea.getContext("2d");
+document.body.insertBefore(gameArea, document.body.childNodes[0]);
