6aa81c201c83afd3ed09cacae29078a328a07618
Sheeva Kavousian <sheevakavous@gmail.com>
2021-01-07 18:54:33 +0000

    Add ability to change direction based on key press

--- script.js
+++ script.js
@@ -15,8 +15,6 @@ function refresh () {
 
 // Generates a square food component at a random position
 function randomFood () {
-  let food
-
   // this do while condition ensures the food is not located within the body of the snake
   do {
     let foodx = Math.floor(Math.random() * 50) * 10;
@@ -27,12 +25,44 @@ function randomFood () {
   component(10, 10, "yellow", food.x, food.y)
 }
 
-// Moves the snake along one unit each second
-function move () {
-  snake = snake.map(function (i) {
-    return {x:(i["x"] + 10), y:i["y"]}
-  });
-  refresh()
+// changes direction of motion depending on key pressed
+function direction (event) {
+  const leftKey = 37;
+  const rightKey = 39;
+  const upKey = 38;
+  const downKey = 40;
+
+  const keyPressed = event.keyCode
+   if (keyPressed === leftKey) {
+     movex = -10;
+     movey = 0;
+   } else if (keyPressed === rightKey) {
+     movex = 10;
+     movey = 0;
+   } else if (keyPressed === upKey) {
+     movex = 0;
+     movey = -10;
+   } else if (keyPressed === downKey) {
+     movex = 0;
+     movey = 10;
+   }
+}
+
+// Main function of game
+function main () {
+
+  front = {x:snake[0].x + movex, y:snake[0].y + movey}
+  snake.unshift(front);
+  snake.pop();
+
+
+  if (snake[0]["x"] == food.x && snake[0]["y"] == food.y){
+    refresh()
+    randomFood()
+  } else {
+    refresh()
+    component(10, 10, "yellow", food.x, food.y)
+  }
 }
 
 // Creating the canvas element
@@ -42,12 +72,13 @@ gameArea.height = 500;
 gameArea.style.background = "black";
 let context = gameArea.getContext("2d");
 document.body.insertBefore(gameArea, document.body.childNodes[0]);
-
+document.addEventListener("keydown", direction)
 
 // Creating the initial snake
-let snake = [ {x:200, y:250}, {x:210, y:250}, {x:220, y:250}, {x:230, y:250}, {x:240, y:250}];
-let snakenew
+let snake = [ {x:240, y:250}, {x:230, y:250}, {x:220, y:250}, {x:210, y:250}, {x:200, y:250}];
 
-refresh()
-randomFood()
-window.setInterval(move, 1000);
+let food;
+let head
+refresh();
+randomFood();
+window.setInterval(main, 1000);
