f5de78f34b2142686db66a43571de244a6e99962
Sheeva Kavousian <sheevakavous@gmail.com>
2021-01-08 15:14:16 +0000

    Add if statement for different scenario

--- script.js
+++ script.js
@@ -50,23 +50,33 @@ function direction (event) {
 
 // Main function of game
 function main () {
-
   front = {x:snake[0].x + movex, y:snake[0].y + movey}
-  snake.unshift(front);
-  snake.pop();
 
-
-  if (snake[0]["x"] == food.x && snake[0]["y"] == food.y){
-    refresh()
-    randomFood()
+  // This if statement controls what happens in each different scinarios
+  if (front.x == food.x && front.y == food.y){
+    // if eat food
+    foodCounter += 1;
+    console.log(foodCounter)
+    snake.unshift(front);
+    refresh();
+    randomFood();
+  } else if (front.x === -10 || front.x === 500 || front.y === -10 || front.y === 500) {
+    // if hit walls
+    console.log("Warning!!!");
+  } else if (snake.some(i => i.x == front.x && i.y == front.y)) {
+    // if hit snake
+    console.log("hey");
   } else {
-    refresh()
-    component(10, 10, "yellow", food.x, food.y)
+    // if doesn't hit anything
+    snake.unshift(front);
+    snake.pop();
+    refresh();
+    component(10, 10, "yellow", food.x, food.y);
   }
 }
 
 // Creating the canvas element
-let gameArea = document.createElement("canvas");
+const gameArea = document.createElement("canvas");
 gameArea.width = 500;
 gameArea.height = 500;
 gameArea.style.background = "black";
@@ -77,8 +87,13 @@ document.addEventListener("keydown", direction)
 // Creating the initial snake
 let snake = [ {x:240, y:250}, {x:230, y:250}, {x:220, y:250}, {x:210, y:250}, {x:200, y:250}];
 
+// declaring functions
 let food;
-let head
+let head;
+let movex = 10;
+let movey = 0;
+let foodCounter = 0;
+
 refresh();
 randomFood();
-window.setInterval(main, 1000);
+window.setInterval(main, 200);
